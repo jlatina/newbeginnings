b2ed149b86a55eb0a97bace4078c96776c1a87db
jlatina <jivillan@bu.edu>
2023-04-22 17:57:39 -0400

    added db connection code from other project for smooth transition to add into database

--- back_end/db.py
+++ back_end/db.py
@@ -1,6 +1,43 @@
 # Copyright 2023 Jeanette Villanueva 
+import mysql.connector
+from mysql.connector import Error  
+# https://pynative.com/python-mysql-database-connection/#h-how-to-connect-mysql-database-in-python
 
-""" 
-File to detail the sql database & will eventually incoroporate how the user's data will move to the cloud for security
 
+
+"""method to connect to data base 
+    return connection_status: whether or not the user was able to succesfully connect 
+     status_code = 100 : success
+     status_code = 400 : failure 
 """
+
+def db_connect():
+    connection = None
+    try:
+        connection = mysql.connector.connect(user = 'root', host='localhost',
+                                            database= 'fy_app_db')
+        if connection.is_connected():
+            mysql_version = connection.get_server_info()
+            print("Connected to MySQL Server version ", mysql_version)
+            cursor = connection.cursor()
+            cursor.execute("select database();")
+            db_name = cursor.fetchone()
+            print("You're connected to database: ", db_name)
+            status_code = 100 
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+        status_code = 400
+    finally:
+        if connection is not None and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+    return status_code         
+
+
+def main():
+    result = db_connect()
+    print(result)
+if __name__=="__main__":
+    main()
